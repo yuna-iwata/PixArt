dd4d9fbab01c1b520c75a5734ce8dc8044d8389b
Yuna Iwata <115478706+yuna-iwata@users.noreply.github.com>
2023-10-25 00:12:57 +0100

    feat: google log in working now

--- src/LoginScreen.tsx
+++ src/LoginScreen.tsx
@@ -9,25 +9,17 @@ import auth from '@react-native-firebase/auth';
 
 export default function LoginScreen() {
   const [loggedIn, setloggedIn] = useState(false);
-  const [userInfo, setuserInfo] = useState([]);
 
   const signIn = async () => {
     try {
-      console.log('here1');
       await GoogleSignin.hasPlayServices();
-      console.log('here2');
-      const googleUserInfo = await GoogleSignin.signIn();
-      console.log('here3');
-      const token = await GoogleSignin.getTokens();
-      // console.log('here4');
-      setuserInfo({googleUserInfo});
-      console.log('here5');
+      const {accessToken, idToken} = await GoogleSignin.signIn();
       setloggedIn(true);
-      console.log('here6');
-      const credential = auth.GoogleAuthProvider.credential(token);
-      console.log('here7');
+      const credential = auth.GoogleAuthProvider.credential(
+        idToken,
+        accessToken,
+      );
       await auth().signInWithCredential(credential);
-      console.log('here8');
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
         // user cancelled the login flow
